4ae49770cfcaf39d3a139eea13c5dae081287c1b
zhanlong <zhzhl1993@163.com>
2016-11-02 22:24:29 +0800

    发微博相册初步完成

---
+++ 微博/Classes/Compose/View/WBComposePhotosView.h
@@ -0,0 +1,13 @@
+//
+//  WBComposePhotosView.h
+//  微博
+//
+//  Created by 朱占龙 on 2016/11/2.
+//  Copyright © 2016年 cuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBComposePhotosView : UIView
+
+@end
---
+++ 微博/Classes/Compose/View/WBComposePhotosView.m
@@ -0,0 +1,21 @@
+//
+//  WBComposePhotosView.m
+//  微博
+//
+//  Created by 朱占龙 on 2016/11/2.
+//  Copyright © 2016年 cuit. All rights reserved.
+//
+
+#import "WBComposePhotosView.h"
+
+@implementation WBComposePhotosView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
